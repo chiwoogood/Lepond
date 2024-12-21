22f2bbe11facae57670064e601ef0954141ae62d
chiwoo <chiwooooo@naver.com>
2024-12-21 21:44:13 +0900

    local

--- static/js/base.js
+++ static/js/base.js
@@ -5,25 +5,3 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const wrapper = document.querySelector('#barba-wrapper');
-    console.log('Wrapper found:', wrapper); // 디버깅용 로그
-
-    if (wrapper) {
-        barba.init({
-            transitions: [{
-                name: 'fade',
-                leave(data) {
-                    console.log('Leaving:', data.current.container); // 디버깅 로그
-                    return gsap.to(data.current.container, { opacity: 0 });
-                },
-                enter(data) {
-                    console.log('Entering:', data.next.container); // 디버깅 로그
-                    return gsap.from(data.next.container, { opacity: 0 });
-                }
-            }]
-        });
-    } else {
-        console.error('No Barba wrapper found in the DOM.');
-    }
-});
