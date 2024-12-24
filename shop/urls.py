0b406ce102a60e2cac82d364801def2c350e22f3
chiwoo <chiwooooo@naver.com>
2024-12-24 16:29:43 +0900

    detail

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,5 @@ app_name = 'shop'  # 네임스페이스 설정
 
 urlpatterns = [
     path('', views.items, name='items'),
+    path('details', views.details, name='details'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def items(request):
     return render(request,'shop/items.html')
+
+def details(request):
+    return render(request, 'shop/details.html')
