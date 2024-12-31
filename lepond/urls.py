828533b36af3dff1bd4f6da8a947d66190d75a17
chiwoo <chiwooooo@naver.com>
2024-12-31 11:42:02 +0900

    404

--- lepond/urls.py
+++ lepond/urls.py
@@ -18,6 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
+
+
+def custom_404_test_view(request):
+    return render(request, '404.html', status=404)
+
+def custom_500_test_view(request):
+    return render(request, '500.html', status=500)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
