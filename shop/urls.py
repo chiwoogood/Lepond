94185d1bbca4e9053c20f5f843e07150c4750e63
chiwoo <chiwooooo@naver.com>
2025-03-16 16:55:11 +0900

    product pagination

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'shop'  # 네임스페이스 설정
+app_name = 'shop'
 
 urlpatterns = [
     path('', views.items, name='items'),
-    path('details/', views.details, name='details'),
+    path('details/<int:pk>/', views.details, name='details'),
     path('cart/', views.cart, name='cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
 def items(request):
-    return render(request,'shop/items.html')
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 8)  
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+    
+    context = {
+        'products' : products,
+    }
+    return render(request, 'shop/items.html', context)
 
-def details(request):
-    return render(request, 'shop/details.html')
+# def QnA(request):
+#     page = request.GET.get('page', 1) 
+#     qna_list = Qna.objects.all().order_by('-created_at')  
+#     qna_info_list = QnaInfo.objects.all().order_by('-created_at') 
+
+#     paginator = Paginator(qna_list, 10)  
+#     qna_page = paginator.get_page(page)  
+
+#     context = {
+#         "QnAs": qna_page,  
+#         "QnaInfos": qna_info_list,
+#     }
+#     return render(request, 'community/QnA.html', context)
+
+
+def details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product' : product
+    }
+    return render(request, 'shop/details.html',context)
 
 def cart(request):
     return render(request, 'shop/cart.html')
+
+
